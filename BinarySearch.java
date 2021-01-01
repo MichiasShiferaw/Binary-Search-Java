59881cf86f44c555a1e8d7c441fa4cbda348422b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-01 17:28:15 -0500

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,8 @@
-/have to import the following:
+//have to import the following:
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
